db6f85a9e526b17bfb75fd128980f64b0c19d666
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-02-23 07:41:07 -0500

    spline init

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Spline from "@splinetool/react-spline";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Banner />
+    <div className="flex w-screen h-screen items-center justify-center relative bg-primary pb-20">
+      <Spline scene="https://prod.spline.design/mN6pfn4E4QGERrzW/scene.splinecode" />
     </div>
   );
 }
